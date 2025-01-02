44e2896258d298b78ef5064f48a3299b159c0261
Ryan Ka Yau Lai <43101723+kayaulai@users.noreply.github.com>
2025-01-01 17:53:16 -0800

    Fix filepaths for analysis data

--- src/02_integrate_changes.R
+++ src/02_integrate_changes.R
@@ -26,7 +26,7 @@ updateDF = function(discoName){
 
 saveUpdatedDF = function(discoName, newDF){
   rez_save(newDF, here("data", "02_rezrDF", glue("{discoName}.Rdata")))
-  rez_write_csv(newDF, here("output", "analysis_data", glue("{discoName}.csv")),
+  rez_write_csv(newDF, here("output", "02_final_data", glue("{discoName}.csv")),
                 c("id", "name", "unitLastWord", "unitSeqLast", "word",
                   "argOrder", "noPrevMentions", "noPrevZero", "noNextMentions", "noNextZero",
                   "bridge", "justFirst", "justLast", "local", "identifiable", "topic",
