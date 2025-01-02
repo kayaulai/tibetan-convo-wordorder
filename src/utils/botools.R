e5b5d49b163544f0291da8a919cd3c7765f542b8
Ryan Ka Yau Lai <43101723+kayaulai@users.noreply.github.com>
2025-01-01 17:54:58 -0800

    stringr:: in botools

--- src/utils/botools.R
+++ src/utils/botools.R
@@ -1,14 +1,14 @@
 cleanTrailingTsheg = function(strings){
-  case_when(str_ends(strings, "་") ~ substr(strings, 1, nchar(strings) - 1),
+  case_when(stringr::str_ends(strings, "་") ~ substr(strings, 1, nchar(strings) - 1),
             T ~ strings)
 }
 
 cleanTrailingSpace = function(strings){
-  case_when(str_ends(strings, " ") ~ substr(strings, 1, nchar(strings) - 1),
+  case_when(stringr::str_ends(strings, " ") ~ substr(strings, 1, nchar(strings) - 1),
             T ~ strings)
 }
 
 cleanLeadingTsheg = function(strings){
-  case_when(str_starts(strings, "་") ~ substr(strings, 2, nchar(strings)),
+  case_when(stringr::str_starts(strings, "་") ~ substr(strings, 2, nchar(strings)),
             T ~ strings)
 }
